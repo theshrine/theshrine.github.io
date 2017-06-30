fa2c2f92b2d7dcc81a135baa9834d2e2aeaf3be0
Suman Senapati <sigma.6.2009@gmail.com>
2017-06-30 16:43:28 +0000

    added basic layout to home page

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    var ht=$('.block').height();
+    $(window).scroll(function(){
+       var scroll=$(window).scrollTop();
+       //alert(scroll);
+       if(scroll>=(ht))
+       {
+           $('#navbar').css('background-color','#ECF0F1');
+       }
+       else if(scroll<ht)
+       {
+           $('#navbar').css('background-color','#2C3E50');
+       }
+    });
+    
+    
+});
